4c53cf201793c7a26c871966ac899c2aa53a1d27
vijay rampogu <vijayrampogu@gmail.com>
2018-07-22 18:22:12 -0400

    initial commit

---
+++ src/test/java/com/test/TC01.java
@@ -0,0 +1,26 @@
+package com.test;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC01 {
+	
+	@Test
+	public void tc01_1(){
+	
+		//Assert.fail();
+	}
+	@Test
+      public void tc01_2(){
+		
+		Assert.fail();
+	}
+	@Test
+      public void tc01_3(){
+		Assert.fail();
+	
+}
+
+
+}
---
+++ src/test/java/com/test/TC02.java
@@ -0,0 +1,22 @@
+package com.test;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC02 {
+	
+	@Test
+	public void tc02_1(){
+	
+		//Assert.fail();
+	}
+	@Test
+      public void tc02_2(){
+		Assert.fail();
+	}
+	@Test
+      public void tc02_3(){
+		//Assert.fail();
+	
+}
+
+}
